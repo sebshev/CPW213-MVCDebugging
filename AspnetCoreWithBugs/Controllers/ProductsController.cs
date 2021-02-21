09d3eb68f45140f30f71aa2c457a433a09de3efb
Sebastian Shevlin <seb112697@gmail.com>
2021-02-20 17:02:59 -0800

    Delete is working correctly. All CRUD functional

--- AspnetCoreWithBugs/Controllers/ProductsController.cs
+++ AspnetCoreWithBugs/Controllers/ProductsController.cs
@@ -83,7 +83,8 @@ namespace AspnetCoreWithBugs.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var product = await _context.Product.FindAsync(id);
-            _context.Product.Remove(product);
+            _context.Entry(product).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
